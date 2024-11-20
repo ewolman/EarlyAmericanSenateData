dd7bfabfac1da77503a7c31299ac5dbc0e871d02
elias <elswlmn@gmail.com>
2024-11-19 22:25:07 -0500

    Untested changes to `comb_data.py` to handle duplicate last names in same congress and store corrections to a dictionary to avoid repeated work

--- comb_data.py
+++ comb_data.py
@@ -6,15 +6,40 @@ from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 import sys
 
-# update_text - function to update given page's text file with correct name
-def update_text(congress, pg, old_name, new_name):
-   path = 'scans_and_text/' + congress + '_Congress/Text/Edited/' + congress + '_Congress_p' + pg + '.txt'
-   with open(path, 'r') as f:
-      txt = f.read()
-   txt_update = txt.replace(old_name, new_name)
-   with open(path, 'w') as f:
-      f.write(txt_update)
-   return 'Updated the ' + congress + 'Congress pg ' + pg + ' file'
+# update_text - functions to update given page's text file with correct name
+# Includes dictionary to record changes
+try:
+   with open('name_changes.json', 'r') as f:
+      name_changes = json.load(f)
+except FileNotFoundError:
+   name_changes = {}
+
+# function to add correction to dictionary
+def add_correction(name_changes, corrections, congress, old_name, new_name):
+   if congress not in name_changes: # check if changes includes given congress
+      name_changes[congress] = {}   # if not - create new dictionary for given congress
+   # check if the last name appears more than once in said congress
+   name_changes[congress][old_name] = new_name # add a new correction 
+   # save file
+   with open('name_changes.json', 'w') as f:
+      json.dump(name_changes, f, indent = 4) # indent for readability
+   return corrections.append(new_name) # append to corrections list
+
+# function to get correction for a given name
+def get_correction(name_changes, congress, name):
+   return name_changes.get(str(congress), {}).get(name, None) # returns empty dict, None for nonexistent congresses/Names
+
+#function to check and choose duplicate
+def check_duplicate(duplicates, info, data, congress, old_name, new_name):
+   if new_name in duplicates:
+      # get full names of senators with same last name 
+      dupes = (info[(info['congressNumber'] == congress) & (info['unaccentedFamilyName'].isin(duplicates))]['givenName'])
+      print(dupes)
+      d = input('Pick first name from list above: ')
+      data.loc[(data['NAME'] == old_name), ['first']] = dupes[d]
+      return data
+   else:
+      return data
 
 # read in info data
 info = pd.read_csv('Info.csv')
@@ -61,7 +86,9 @@ info['age'] = info['congressYear'] - info['birthYear']
 
 # make names lowercase for easier matching
 info['unaccentedFamilyName'] = info['unaccentedFamilyName'].str.lower()
-# take party out of it's list format
+# join first and last to create a full name column
+info['fullName'] = info['givenName'] + ' ' + info['unaccentedFamilyName']
+# take party out of its list format
 info['parties'] = [p[0] for p in info['parties']]
 
 # write to csv
@@ -74,26 +101,29 @@ skips  = pd.read_csv('skip_log.csv') # skippable names in a csv
 vote_data = os.listdir('vote_data')
 #print(vote_data)
 for congress in vote_data:
+   data = pd.read_csv('vote_data/' + congress)
    #read in as text so needs to be converted
    c = int(congress[0]), congress[0] # int and str 
-   # entries of only correct names for given congress
+
+   # entries of correct names for given congress
    congress_names = info[info['congressNumber'] == c[0]]['unaccentedFamilyName'] 
-   # list of names that appear twice
-   
-   dupes = info[info['congressNumber'] == c[0]]['unaccentedFamilyName'].value_counts() > 1
-   print(c)
-   data = pd.read_csv('vote_data/' + congress)
-   # lowercase name
+   full_names = info[info['congressNumber'] == c[0]]['fullName']
+
+   # find duplicate names by finding all names that appear more than once for a given congress
+   duplicates = info[info['congressNumber'] == c[0]]['unaccentedFamilyName'].value_counts() > 1
+   duplicates = duplicates[duplicates].index # save a list of these names
+
+   # lowercase names
    data['NAME'] = data['NAME'].str.lower()
+   data['first'] = None # create a first name column for cases of duplicate last name
    #print(data.head())
 
-   # check for mistakes in data and change them
+   # check for mistakes in data and get a list of non_matches to the info csv
    nonmatches = data['NAME'][~data['NAME'].isin(congress_names)].unique()
-   corrections = []
+   corrections = [] # empty list to record corrections
    i = 0 # index
    for name in nonmatches: #iterate through unique nonmatches
-      print(name)
-      pgs = data[data['NAME'] == name]['PAGE']
+      #print(name)
       # check if name can be skipped
       if sum(skips.iloc[:,0].str.fullmatch(name)) > 0: #check if name is in skip csv
          print(name)
@@ -101,50 +131,59 @@ for congress in vote_data:
          nonmatches = np.delete(nonmatches, i) # delete from nonmatch list
          continue #keep running loop
 
-      closest_matches = process.extract(name, congress_names, limit = 2) # find 2 closest matches
-      # if the match score is above 80, append
-      if closest_matches[0][1] > 80:
-         new_name = closest_matches[0][0]
-         corrections.append(new_name)
-         for p in pgs:
-            update_text(c[1], str(p), name, new_name)
-      # else: ask for input to look at matches
-      else:
-         print(data[data['NAME'] == name]) # print mistake
-         # open documents and ask for input
-         print('Pick closest match ' + name + str(closest_matches) + ' (1 or 2, 3 if wrong): ')
-         pg = str(data[data['NAME'] == name]['PAGE'].iloc[0])
-         os.system('start scans_and_text/' +  c[1] + '_Congress/Scans/' + c[1] + '_Congress_p' + pg + '.png' 
-                    + '&& notepad scans_and_text/' + c[1] + '_Congress/Text/Edited/' + c[1] + '_Congress_p' + pg) 
-         m = int(input())
-         if m == 1 or m == 2:
-            # append based on choice - 1
-            new_name = closest_matches[m-1][0]
-            corrections.append(new_name)
-            for p in pgs:
-               update_text(c[1], str(p), name, new_name)
-         elif m == 3:
+      # check if correction exists
+      correction = get_correction(name_changes, c[0], name)
+      if correction: 
+         corrections.append(correction) # append to corrections list
+         data = check_duplicate(duplicates, info, data, congress, name, new_name)
 
-            # if no mistake, then delete nonmatch i from list, and add to file to skip in future
-            # if mistake is fixable - ask for new name - change in txt file and vote csv
-            q = input('1: Add to data without correction; 2: Name needs to be changed; 3: Error; : ')
-            if  int(q) == 1:
-               nonmatches = np.delete(nonmatches, i)
-               skips = skips.append(name)
-               skips.to_csv('skip_log.csv', index = False)
-            elif int(q) == 2:
-               new_name = input('Corrected name: ')
-               corrections.append(new_name)
-               for p in pgs:
-                  update_text(c[1], str(p), name, new_name)
-            else: 
-               print('Something needs to be changed in the files, writing name to error file ....')
-               # writes wrong name and data to a log file
-               with open('error_log.txt', 'a') as e:
-                  e.write(str(data[data['NAME'] == name]))
+      # find correction
+      else:
+         pgs = data[data['NAME'] == name]['PAGE'] # record page appearances of incorrect name
+
+         closest_matches = process.extract(name, congress_names, limit = 2) # find 2 closest matches
+         # if the match score is above 90, append without asking
+         if closest_matches[0][1] > 90: # threshold needs to be high because we can't match S Foster to Foster etc.
+            new_name = closest_matches[0][0]
+            # update corrections dictionary file
+            corrections = add_correction(name_changes, corrections, c[0], name, new_name) 
+            data = check_duplicate(duplicates, info, data, congress, name, new_name)
+         
+         # else: ask for input to look at matches
          else:
-            print('Something is wrong here')
-            sys.exit(1)
+            print(data[data['NAME'] == name]) # print mistake
+            # open documents and ask for input
+            print('Pick closest match ' + name + str(closest_matches) + ' (1 or 2, 3 if wrong): ')
+            pg = str(data[data['NAME'] == name]['PAGE'].iloc[0])
+            os.system('start scans_and_text/' +  c[1] + '_Congress/Scans/' + c[1] + '_Congress_p' + pg + '.png' 
+                    + '&& notepad scans_and_text/' + c[1] + '_Congress/Text/Edited/' + c[1] + '_Congress_p' + pg) 
+            m = int(input())
+            if m == 1 or m == 2:
+               # append based on choice - 1 is index 0, 2 is index 1
+               new_name = closest_matches[m-1][0]
+               corrections = add_correction(name_changes, corrections, c[0], name, new_name)
+               data = check_duplicate(duplicates, info, data, congress, name, new_name)
+
+            elif m == 3:
+               # if no mistake, then delete nonmatch i from list, and add to file to skip in future
+               # if mistake is fixable - ask for new name - change in txt file and vote csv
+               q = input('1: Add to data without correction; 2: Name needs to be changed; 3: Error; : ')
+               if  int(q) == 1:
+                  nonmatches = np.delete(nonmatches, i)
+                  skips = skips.append(name)
+                  skips.to_csv('skip_log.csv', index = False)
+               elif int(q) == 2:
+                  new_name = input('Corrected name: ')
+                  corrections = add_correction(name_changes, corrections, c[0], name, new_name)
+                  data = check_duplicate(duplicates, info, data, congress, name, new_name)
+               else: 
+                  print('Something needs to be changed in the files, writing name to error file ....')
+                  # writes wrong name and data to a log file
+                  with open('error_log.txt', 'a') as e:
+                     e.write(str(data[data['NAME'] == name]))
+            else:
+               print('Something is wrong here')
+               sys.exit(1)
       i += 1
    # replace incorrect nonmatches from data sheet and save in updated folder
    #print(nonmatches, corrections)   
